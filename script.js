b9db49b288f691775ee0152a452e1bafd40b7ed2
Nokky07 <kieranmcm2007@gmail.com>
2024-03-22 23:24:49 +0000

    push

---
+++ script.js
@@ -0,0 +1,11 @@
+const typed = new Typed(".auto-type", {
+  strings: [
+    "GameInfo",
+    "GameInfo.com",
+    "Gaming Information.",
+    "Games."
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
