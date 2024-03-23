0acca47efc77010d7d1f1c5620cc096b81813602
Nokky07 <kieranmcm2007@gmail.com>
2024-03-23 23:51:51 +0000

    push

--- script.js
+++ script.js
@@ -10,3 +10,9 @@ const typed = new Typed(".auto-type", {
   loop: true,
 
 });
+
+console.log("Welcome to GameInfo.com Console");
+
+console.log("Type 'help' for a list of commands.");
+
+console.log("Type 'about' for information about GameInfo.");
