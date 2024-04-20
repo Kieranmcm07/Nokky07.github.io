e976bd32262f81a4106ed689a847997e4e78fcb7
Nokky07 <kieranmcm2007@gmail.com>
2024-04-20 21:27:46 +0000

    push

--- top.js
+++ top.js
@@ -1,10 +1,19 @@
-const getUserSelectedText = () => {
-  return window.getSelection().toString();
-}
+// MIT License
 
-window.onscroll = () => {
-  if((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
-    console.log("Bottom of page reached!");
+// Copyright (c) 2024 Nokky07
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// Taking user back to top smoothly.
+
+const toTop = () => {
+  const c = document.documentElement.scrollTop || document.body.scrollTop;
+  if (c > 0) {
+    window.requestAnimationFrame(toTop);
+    window.scrollTo(0, c - c / 8);
   }
-}
-
+};
